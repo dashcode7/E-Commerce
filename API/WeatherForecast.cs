d1c20ed88854ce6a30f8cfe96023f5052468bd4d
Kiran <Kiran@LAPTOP-1VOFLUHF>
2023-07-18 13:19:07 +0530

    devbranch test

--- API/WeatherForecast.cs
+++ API/WeatherForecast.cs
@@ -4,11 +4,11 @@ namespace API
     {
         public DateTime Date { get; set; }
 
-        public int TemperatureC{ get; set; }
+        public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
-        public string? samples { get; set; }
+        public string? samp { get; set; }
     }
 }
