dd9b71e2a57783424f9ba757f56f7c0490e329cd
Kiran <Kiran@LAPTOP-1VOFLUHF>
2023-07-18 13:01:28 +0530

    First Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController:ControllerBase
+    {
+        private readonly StoreContextcs _context;
+        public ProductsController(StoreContextcs context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var result = await _context.Products.ToListAsync();
+            return result;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetName(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
---
+++ Infrastructure/Data/StoreContextcs.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextcs : DbContext
+    {
+        public StoreContextcs(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
